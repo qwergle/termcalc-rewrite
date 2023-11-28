c6e3adcde1fc8b3c94ee5869baaaf0cd18ad31eb
supremestdoggo <83146042+supremestdoggo@users.noreply.github.com>
2023-11-28 18:23:20 +0000

    Edit UI (again)

--- main.c
+++ main.c
@@ -155,14 +155,26 @@ void print_graph(VM_Code func, graphing_options opts) {
   print_grid(render_function(func, opts), opts.width, opts.height);
 }
 
-#define QUERY_VALUE_FROM_USER(prompt, format_specifier, pointer) do { \
-  fputs(prompt, stdout); \
-  while (scanf(format_specifier, pointer) != 1) { \
-    fputs("Invalid input. Try again.\n", stdout); \
-  } \
+#define DIFFERENT_SCANF(format, ...) do { \
+  char *input_string = getstr(); \
+  rc = sscanf(input_string, format, __VA_ARGS__); \
 } while (0)
 
+void query_value_from_user(char *prompt, char *format_specifier, void *pointer) {
+  fputs(prompt, stdout);
+  fflush(stdout);
+  int rc;
+  DIFFERENT_SCANF(format_specifier, pointer);
+  while (rc != 1) {
+    fputs("Invalid input. Try again. ", stdout);
+    fflush(stdout);
+    DIFFERENT_SCANF(format_specifier, pointer);
+  }
+}
+
 int main(void) {
+  fputs("f(x) = ", stdout);
+  fflush(stdout);
   size_t tok_length = 32;
   char *str = getstr();
   Token *tokens = tokenizer(str, &tok_length);
@@ -172,11 +184,12 @@ int main(void) {
   VM_Code code = compile(top_node);
 
   graphing_options opts;
-  QUERY_VALUE_FROM_USER("Please input the width of the grid... ", "%lu", &(opts.width));
-  QUERY_VALUE_FROM_USER("Please input the height of the grid... ", "%lu", &(opts.height));
-  QUERY_VALUE_FROM_USER("Please input the center of the X axis... ", "%lf", &(opts.x_offset));
-  QUERY_VALUE_FROM_USER("Please input the center of the Y axis... ", "%lf", &(opts.y_offset));
-  QUERY_VALUE_FROM_USER("Please input the scale... ", "%lf", &(opts.scale));
+  query_value_from_user("Please input the width of the grid... ", "%lu", &(opts.width));
+  query_value_from_user("Please input the height of the grid... ", "%lu", &(opts.height));
+  opts.height /= 2;
+  query_value_from_user("Please input the center of the X axis... ", "%lf", &(opts.x_offset));
+  query_value_from_user("Please input the center of the Y axis... ", "%lf", &(opts.y_offset));
+  query_value_from_user("Please input the scale... ", "%lf", &(opts.scale));
   opts.mode = 0;
   print_graph(code, opts);
   return 0;
